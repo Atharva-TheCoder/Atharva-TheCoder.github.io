17ccf434ce4c82573834850be91bd40615e034e5
Kali Live user <thecoolscience1820@gmail.com>
2021-07-03 17:34:50 +0000

    webpage contents

--- main/js/cookie-consent.js
+++ main/js/cookie-consent.js
@@ -5,13 +5,13 @@ acceptBtn = cookieBox.querySelector("button");
 
 acceptBtn.onclick = ()=>{
 //setting cookie for 1 month, after one month it'll be expired automatically
-document.cookie = "CookieBy=CodingNepal; max-age="+60*60*24*30;
+document.cookie = "The_Cool_Science_Cookies=cookie.DataBase_Savedata_thecoolscience; max-age="+60*60*24*30;
 if(document.cookie){ //if cookie is set
 cookieBox.classList.add("hide"); //hide cookie box
 }else{ //if cookie not set then alert an error
 alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
 }
 }
-let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
+let checkCookie = document.cookie.indexOf("The_Cool_Science_Cookies=cookie.DataBase_Savedata_thecoolscience"); //checking our cookie
 //if cookie is set then hide the cookie box else show it
 checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
